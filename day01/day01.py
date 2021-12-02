c23ed16dc0697dc4142b733feda3b74a2f3f7c02
francois07 <francois.soulie@edu.esiee.fr>
2021-12-02 13:20:51 +0100

    Revert "Added README to Day2"
    This reverts commit 7c2f462c9582a3f1046ae88eae421e0d5970800b.

--- day01/day01.py
+++ day01/day01.py
@@ -1,16 +1,22 @@
-def window_measurements(data: str, window_size: int = 1) -> int:
-    """
-    Counts how many times the given window sum decreases
+input = [int(x) for x in open("input.txt").read().splitlines()]
 
-    :param str data: The data to compute
-    :param int window_size: The window size
-    """
-    parsed_data = [int(x) for x in data.splitlines()]
+
+def count_decrease(data: list[int]) -> int:
     n = 0
 
-    for i in range(0, len(parsed_data) - window_size):
-        A = (parsed_data[i+k] for k in range(window_size))
-        B = (parsed_data[i+k+1] for k in range(window_size))
+    for i in range(1, len(data)):
+        if data[i-1] < data[i]:
+            n += 1
+
+    return n
+
+
+def window_measurements(data: list[int], window_size: int) -> int:
+    n = 0
+
+    for i in range(0, len(data) - window_size):
+        A = (data[i+k] for k in range(window_size))
+        B = (data[i+k+1] for k in range(window_size))
 
         if sum(A) < sum(B):
             n += 1
@@ -18,15 +24,5 @@ def window_measurements(data: str, window_size: int = 1) -> int:
     return n
 
 
-def main():
-    INPUT_TEXT = open("input.txt")
-
-    decrease_count = window_measurements(INPUT_TEXT)
-    window_decrease_count = window_measurements(INPUT_TEXT, 3)
-
-    print("Part1:", decrease_count)
-    print("Part2:", window_decrease_count)
-
-
-if __name__ == "__main__":
-    main()
+print("Part1:", count_decrease(input))
+print("Part2:", window_measurements(input, 3))
